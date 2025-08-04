03d929f6b44014c1808933e344bb7ba92bbebbf2
Jonathan Gwilliams <jonathan.gwilliams@cliksoftware.com>
2025-08-04 10:11:18 +0100

    Initial Commit

---
+++ Fetch/FetchApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchApp.swift
+//  Fetch
+//
+//  Created by Jonathan Gwilliams on 04/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct FetchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
